b741180de7911804fb0b8bc53ea6b09252799988
Kalkidan Alemu <kali.taye@gmail.com>
2021-06-25 14:47:26 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "../src/assets/nyt-logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <img src={logo} className="logo" alt="New York Times Logo" />
+      <h3 className="section"> Choose a Section</h3>
+      <div className="selectOptions">
+        <select class="select-menu">
+          <option value=""> Sections...</option>
+          <option value="home"> Home</option>
+          <option value="arts"> Arts</option>
+          <option value="business"> Business</option>
+          <option value="sports"> Sports</option>
+          <option value="technology"> Technology</option>
+          <option value="fashion"> Fashion</option>
+        </select>
+      </div>
+      <h3>&copy;Copyright 2021 INSTANEWS</h3>
     </div>
   );
 }
